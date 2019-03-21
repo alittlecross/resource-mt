2f5c532ab49f82a710c38ca3c21955070ade66e6
alittlecross <alittlecross@live.co.uk>
2019-03-21 16:52:16 +0000

    login route and views created and tested

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const session = require('express-session')
 const app = express()
 const port = process.env.PORT || 3000
 
+const user = require('./lib/user')
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(session({
@@ -21,6 +23,21 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { sessionFlash: res.locals.sessionFlash })
+})
+
+app.post('/log-in', async (req, res) => {
+  let result = await user.logIn(req.body)
+  if (result.status) {
+    req.session.user = result.user
+    res.redirect('/dashboard')
+  } else {
+    req.session.sessionFlash = { message: result.message }
+    res.redirect('/')
+  }
+})
+
+app.get('/dashboard', async (req, res) => {
+  res.render('dashboard.ejs')
 })
 
 app.listen(port)
---
+++ test/feature/login-test.js
@@ -0,0 +1,47 @@
+const Browser = require('zombie')
+const Helper = require('../test-helpers')
+
+const app = require('../../app')
+const http = require('http')
+
+http.createServer(app)
+
+describe('User visits index page', () => {
+  const browser = new Browser({ site: 'http://localhost:3000' })
+
+  before(async () => {
+    await Helper.changeEnvironment()
+    await Helper.runMigrations()
+    await Helper.truncateDatabase()
+    await Helper.createUser()
+  })
+
+  describe('submit form', () => {
+    it('correct email and password should be successful', async () => {
+      await browser.visit('/')
+      await browser.fill('email', 'michael.scott@scranton.com')
+      await browser.fill('password', 'password')
+      await browser.pressButton('log in')
+
+      browser.assert.url('http://localhost:3000/dashboard')
+    })
+
+    it('incorrect email should be unsuccessful', async () => {
+      await browser.visit('/')
+      await browser.fill('email', 'michael@scranton.com')
+      await browser.fill('password', 'incorrect')
+      await browser.pressButton('log in')
+
+      browser.assert.url('http://localhost:3000/')
+    })
+
+    it('incorrect password should be unsuccessful', async () => {
+      await browser.visit('/')
+      await browser.fill('email', 'michael.scott@scranton.com')
+      await browser.fill('password', 'incorrect')
+      await browser.pressButton('log in')
+
+      browser.assert.url('http://localhost:3000/')
+    })
+  })
+})
