33e011b7d9b7acad78a78d1ff535191d6d2e6478
alittlecross <alittlecross@live.co.uk>
2019-05-08 11:11:17 +0100

    updated person view to allow resource managers to view

--- server/routes/person.js
+++ server/routes/person.js
@@ -1,8 +1,10 @@
 const Form = require('../lib/person/form')
+const Leave = require('../lib/leave')
 const Person = require('../lib/person')
 
 module.exports = async (req, res) => {
-  const results = await Form.options()
-  const result = await Person.getPerson(req.params.personId)
-  res.render('person.ejs', { options: results, person: result, user: req.session.user })
+  const leave = await Leave.getLeave(req.params.personId)
+  const options = await Form.options()
+  const person = await Person.getPerson(req.params.personId)
+  res.render('person.ejs', { balance: leave.balance, options: options, person: person, requests: leave.requests, user: req.session.user })
 }
